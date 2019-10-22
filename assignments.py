103e5d1e02f949e959c02aed6574ecdb9dddbded
Felipe Faria <felipe.a.b.faria@gmail.com>
2019-10-22 10:40:44 -0300

    Created test to same memmory allocation
    This poc shows that same results may have different memory address

---
+++ assignments.py
@@ -0,0 +1,11 @@
+word_diff_memory = -500
+print(id(word_diff_memory))
+
+word_diff_memory2 = -500
+print(id(word_diff_memory2))
+
+word_same_memory = 255
+print(id(word_same_memory))
+
+word_same_memory2 = 255
+print(id(word_same_memory2))
